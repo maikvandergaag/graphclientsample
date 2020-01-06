eb513484c61ce05bf3494c0d28bc05ac8e1743e5
Maik van der Gaag <maik@familie-vandergaag.nl>
2020-01-06 10:42:15 +0100

    initial add

---
+++ GraphClient/AuthenticationHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+using System.Threading.Tasks;
+
+namespace GraphClient
+{
+    public static class AuthenticationHelper
+    {
+        public static async Task<ServiceClientCredentials> GetServiceClientCredentials(string resource, string clientId, string clientSecret, string authority)
+        {
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+
+            AuthenticationResult authResult = await authContext.AcquireTokenAsync(resource, new ClientCredential(clientId, clientSecret));
+
+            string accessToken = authResult.AccessToken;
+
+            ServiceClientCredentials serviceClientCreds = new TokenCredentials(authResult.AccessToken);
+
+            return serviceClientCreds;
+        }
+    }
+}
---
+++ GraphClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Management.ResourceGraph;
+using Microsoft.Azure.Management.ResourceGraph.Models;
+using System;
+using System.Collections.Generic;
+
+namespace GraphClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string clientId = "[clientid]";
+            string clientSecret = "[clientSecret]";
+            string tenantId = "[tenantId]";
+            string subscriptionId = "[subscriptionId]";
+            string query = "Resources | project name, type | limit 5";
+            string authority = $"https://login.microsoftonline.com/{tenantId}";
+
+            ResourceGraphClient client = new ResourceGraphClient(AuthenticationHelper.GetServiceClientCredentials("https://management.core.windows.net", clientId, clientSecret, authority).Result);
+
+            var response = client.Resources(new QueryRequest(new List<string>(){ subscriptionId }, query));
+
+            Console.WriteLine(response);
+        }
+    }
+}
